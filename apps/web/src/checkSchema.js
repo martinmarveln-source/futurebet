f0fd3dc35103bcc66b4ba746a0ecaffa31308f2b
futurebetmatchfilter-dev <futurebetmatchfilter@gmail.com>
2026-08-07 18:06:30 +0100

    chore: remove sensitive test scripts

--- apps/web/src/checkSchema.js
+++
@@ -1,2 +0,0 @@
-const sql = require('./app/api/utils/sql').default;
-sql`SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'user_preferences'`.then(console.log).catch(console.error).finally(()=>process.exit(0));
